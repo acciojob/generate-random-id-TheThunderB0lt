e59376145b5d936f7f3074dfb73d6310da7eed3e
NIKITH KUMAR N <nikithkumarniki160698@gmail.com>
2024-04-03 22:31:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function makeid(l) {
   // write your code here
+	let res="";
+	let char="qwertyuiopasdfghjklmnbvcxzQWERTYUIOPLKJAHSGDFMZNXBCV1029387465"
+	for(let i=0;i<l;i++){
+		res = res + char.charAt(
+			Math.floor(
+				Math.random() * char.length
+			)
+		);
+	}
+	return res;
+	
 }
 
 // Do not change the code below.
